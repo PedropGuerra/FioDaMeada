b1cabe529ea43764c3b053eaf5e0946b1a004855
Pedro Guerra <pedrop.guerra20@gmail.com>
2023-09-28 22:09:00 -0300

    Testing GCP

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_noticias():
     
     args = request.args.get
     # login_database(request.args.get("API_KEY"))
-    # SQL.connect_db(user = "sendpulse", password=request.args.get("API_KEY"))
+    SQL.connect_db(user = "sendpulse", password=request.args.get("API_KEY"))
     
     logging.info(f"Connect DB, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
 
@@ -43,16 +43,16 @@ def get_noticias():
     #     abort(400, "O usuário não possui preferências cadastradas")
 
     logging.info([qtd_noticias, qtd_fakenews, qtd_rodadas, producao])
-    return [qtd_noticias, qtd_fakenews, qtd_rodadas, producao]
+    # return [qtd_noticias, qtd_fakenews, qtd_rodadas, producao]
     
-    # condicoes_dict = {
-    #     "rodadas+noticias+fake": qtd_rodadas and qtd_fakenews and qtd_noticias,
-    #     "only_noticias": qtd_noticias and not qtd_fakenews,
-    #     "only_fake": qtd_fakenews and not qtd_noticias,
-    #     "fake+rodadas": qtd_fakenews and qtd_noticias and not qtd_rodadas,
-    # }
+    condicoes_dict = {
+        "rodadas+noticias+fake": qtd_rodadas and qtd_fakenews and qtd_noticias,
+        "only_noticias": qtd_noticias and not qtd_fakenews,
+        "only_fake": qtd_fakenews and not qtd_noticias,
+        "fake+rodadas": qtd_fakenews and qtd_noticias and not qtd_rodadas,
+    }
     
-    # logging.info(f"Condicoes Dict, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
+    logging.info(f"Condicoes Dict, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
 
     # # return condicoes_dict
 
@@ -103,53 +103,59 @@ def get_noticias():
     # )
 
     # logging.info(f"Select Noticias + Fake News, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
+    
+    with open("noticias.json", "r") as txt:
+        db_noticias = json.loads(txt.read())
+        db_fakenews = json.loads(txt.read())
 
-    # resp_noticias = {}
-    # resp_gabarito = {}
-    # response = {"Noticias": resp_noticias, "Gabarito": resp_gabarito}
+    logging.info(f"Select Noticias + Fake News, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
 
-    # if condicoes_dict["rodadas+noticias+fake"]:
-    #     for rodada in range(1, qtd_rodadas + 1):
-    #         noticias_por_rodada = min(qtd_noticias // qtd_rodadas, len(db_noticias))
-    #         fakenews_por_rodada = min(qtd_fakenews // qtd_rodadas, len(db_fakenews))
+    resp_noticias = {}
+    resp_gabarito = {}
+    response = {"Noticias": resp_noticias, "Gabarito": resp_gabarito}
 
-    #         rodada_noticias = random.sample(
-    #             db_fakenews, fakenews_por_rodada
-    #         ) + random.sample(db_noticias, noticias_por_rodada)
-    #         random.shuffle(rodada_noticias)
+    if condicoes_dict["rodadas+noticias+fake"]:
+        for rodada in range(1, qtd_rodadas + 1):
+            noticias_por_rodada = min(qtd_noticias // qtd_rodadas, len(db_noticias))
+            fakenews_por_rodada = min(qtd_fakenews // qtd_rodadas, len(db_fakenews))
 
-    #         for i, noticia in enumerate(rodada_noticias):
-    #             resp_noticias[f"noticia{i + 1}"] = noticia
-    #             if noticia["fake"] == 1:
-    #                 resp_gabarito[f"rodada{i+1}"] = {
-    #                     "id": noticia["id"],
-    #                     "local": noticia["fake_local"],
-    #                 }
+            rodada_noticias = random.sample(
+                db_fakenews, fakenews_por_rodada
+            ) + random.sample(db_noticias, noticias_por_rodada)
+            random.shuffle(rodada_noticias)
 
-    # elif condicoes_dict["only_noticias"]:
-    #     for i, noticia in enumerate(db_noticias):
-    #         response["Noticias"][f"noticia{i + 1}"] = noticia
+            for i, noticia in enumerate(rodada_noticias):
+                resp_noticias[f"noticia{i + 1}"] = noticia
+                if noticia["fake"] == 1:
+                    resp_gabarito[f"rodada{i+1}"] = {
+                        "id": noticia["id"],
+                        "local": noticia["fake_local"],
+                    }
 
-    # elif condicoes_dict["only_fake"]:
-    #     for i, fake in enumerate(db_fakenews):
-    #         response["Noticias"][f"noticia{i + 1}"] = fake
+    elif condicoes_dict["only_noticias"]:
+        for i, noticia in enumerate(db_noticias):
+            response["Noticias"][f"noticia{i + 1}"] = noticia
 
-    # elif condicoes_dict["fake+rodadas"]:
+    elif condicoes_dict["only_fake"]:
+        for i, fake in enumerate(db_fakenews):
+            response["Noticias"][f"noticia{i + 1}"] = fake
 
-    #     for i, noticia in enumerate(db_noticias + db_fakenews):
-    #         match noticia["fake"]:
-    #             case 1:
-    #                 resp_noticias[f"noticia{i+1}"] = noticia
-    #                 resp_gabarito[f"rodada{i + 1}"] = {
-    #                     "id": noticia["id"],
-    #                     "local": noticia["fake_local"],
-    #                 }
+    elif condicoes_dict["fake+rodadas"]:
 
-    #             case 0:
-    #                 resp_noticias[f"noticia{i+1}"] = noticia
+        for i, noticia in enumerate(db_noticias + db_fakenews):
+            match noticia["fake"]:
+                case 1:
+                    resp_noticias[f"noticia{i+1}"] = noticia
+                    resp_gabarito[f"rodada{i + 1}"] = {
+                        "id": noticia["id"],
+                        "local": noticia["fake_local"],
+                    }
 
-    # else:
-    #     return Response("error", status=400)
+                case 0:
+                    resp_noticias[f"noticia{i+1}"] = noticia
+
+    else:
+        return Response("error", status=400)
 
     # if not "0" in producao:
     #     def atualizar_noticias(db_noticias, db_fakenews, contact_id):
@@ -160,8 +166,8 @@ def get_noticias():
     #         target=atualizar_noticias, args=(db_noticias, db_fakenews, contact_id)
     #     ).start()
 
-    # logging.info(f"Response Criado, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
+    logging.info(f"Response Criado, CPU(%): {psutil.cpu_percent(4)} / RAM(%): {psutil.virtual_memory()[2]} / RAM(GB): {psutil.virtual_memory()[3]/1000000000}")
     
-    # return response
+    return response
     
     
