7937ae033a252d6589c3aafae67ddc215a618e3f
Pedro Guerra <pedrop.guerra20@gmail.com>
2024-02-14 11:38:38 -0300

    Hello World

--- teste.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("response.json", "r") as file:
-    file = json.loads(file.read())
-
-# print(file["Noticias"]["1"]["Noticia1"]["headline"])
-print(file["Noticias"]["2"][0])
